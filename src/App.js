683f0aaaa56eef15f0ff415d2a0c0ac92f37cecd
Ana Edwards <edwards.ana81@gmail.com>
2022-05-13 14:42:09 +0100

    use effect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  //After every component evaluation then this will run.
+  useEffect(() => {
+    const storedUserLoggedInInfo = localStorage.getItem('isLoggedIn');
+
+  if (storedUserLoggedInInfo === '1') {
+    setIsLoggedIn(true);
+  }
+  }, []);
+  //Will only run once because the dependencies in the [] will never change as no dependencies have been declared.
+  ///Once weve logged in even if we refresh it stays logged in.
+
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+    //Sotre data in local storeage in brower.
+    localStorage.setItem('isLoggedIn', '1')
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,30 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
 const Login = (props) => {
+
   const [enteredEmail, setEnteredEmail] = useState('');
   const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  useEffect(() => {
+    setFormIsValid(
+      enteredEmail.includes('@') && enteredPassword.trim().length > 6
+    );
+  }, [enteredEmail, enteredPassword]);
+  //There is a simple rule as to what you put in the [], you add as dependencies, what you're using in your side effect function.
+  //After every login component function execution, it will rerun this useEffect function but only if either setFormIsValid,
+  //or enteredEmail or enteredPassword,changed in the last component rerender cycle.
+
+  //So that's another scenario where useEffect shines. It helps us make sure that we have one code, in one place, instead of as before in multiple places
+  //which reruns, whenever one of the dependencies changed. So it's not just for when a component was created for the first time,but it's equally 
+  //common to use it to rerun logic when certain data, typically some state or some props changed.
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
   };
 
   const validateEmailHandler = () => {
