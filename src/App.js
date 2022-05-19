b34f5589d964cae229a92d94665d9b38aa944763
Ana Edwards <edwards.ana81@gmail.com>
2022-05-19 11:20:02 +0100

    react context api

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ function App() {
       <AuthContext.Provider 
       value={{
         isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler
       }}
       >
       <MainHeader onLogout={logoutHandler} />
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -9,3 +9,6 @@ export default AuthContext;
 
 //To use Context in your applicaiton you need to provide it (All components that are wrapped by it should have access to it) 
 // and then consume it (hook into it, listen to it). 
+
+//In most cases you will use props to pass data to components because props allows you to configure components and make them reuseable. 
+// Only if you have something which you would foward through a lot of components would you use Reacts Context API. 
